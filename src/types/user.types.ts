b9013f36a451229b67906433c39097b72abb2575
mohamed159716 <mohamed159716@yahoo.com>
2022-07-08 01:55:55 +0200

    Added User Model Types

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,11 +1,15 @@
 import { Types } from 'mongoose';
 
 type UserTypes = {
+   _id: Types.ObjectId;
    name: string;
    email: string;
-   password: string;
+   password: string | undefined;
    passwordConfirmation?: string;
    todo?: Types.ObjectId;
+   passwordChangedAt?: Date;
+   passwordResetToken?: string;
+   passwordResetExpires?: Date;
 };
 
 export default UserTypes;
