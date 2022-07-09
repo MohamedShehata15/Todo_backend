2cee422b404221ae79c94a7abfa84a7a0503e47b
mohamed159716 <mohamed159716@yahoo.com>
2022-07-09 15:28:55 +0200

    Added Protected Token Middleware

---
+++ src/controllers/authController.ts
@@ -0,0 +1,65 @@
+import { Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User';
+import catchAsync from '../utils/catchAsync';
+import { AppError } from '../utils/appError';
+import config from '../config';
+import { JwtPayload } from '../types/jwtPayload.types';
+import { UserRequest } from '../types/userRequest.types';
+
+class AuthController {
+   protect = catchAsync(
+      async (req: UserRequest, _res: Response, next: NextFunction) => {
+         // Getting Token
+         let token: string | null = null;
+
+         if (
+            req.headers.authorization &&
+            req.headers.authorization.startsWith('Bearer')
+         ) {
+            token = req.headers.authorization.split(' ')[1];
+         }
+
+         if (!token) {
+            return next(
+               new AppError(
+                  'You are not logged in! Please log in to get access',
+                  401
+               )
+            );
+         }
+
+         // Verification Token.
+         const jwtSecret: string = config.jwtSecret ?? '';
+         const decode = jwt.verify(token, jwtSecret) as JwtPayload;
+
+         // Check if user exists
+         const currentUser = await User.findById(decode.id);
+         if (!currentUser) {
+            return next(
+               new AppError(
+                  'The user belonging to this token does no longer exits',
+                  401
+               )
+            );
+         }
+
+         // issued token after password changed
+         if (currentUser.changedPasswordAfter(decode.iat)) {
+            return next(
+               new AppError(
+                  'User recently changed password! Please log in again',
+                  401
+               )
+            );
+         }
+
+         // Grant access to route
+         req.user = currentUser;
+         next();
+      }
+   );
+}
+
+export default AuthController;
