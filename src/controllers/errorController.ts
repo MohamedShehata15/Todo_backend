1c099bc718954d84b3d3aa4b8d59c646ef194b01
mohamed159716 <mohamed159716@yahoo.com>
2022-07-09 10:38:06 +0200

    Added Production and development error

---
+++ src/controllers/errorController.ts
@@ -0,0 +1,67 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { AppError } from '../utils/appError';
+import config from '../config';
+
+type ErrorType = Error & {
+   statusCode: number;
+   status: string;
+   isOperational: boolean;
+   code: number;
+   keyValue: { email: string };
+};
+
+const sendErrorDev = (err: ErrorType, res: Response) => {
+   res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack
+   });
+};
+
+const sendErrorProd = (err: AppError, res: Response) => {
+   console.log('Sned Error Production');
+
+   // Operational Error
+   if (err.isOperational) {
+      res.status(err.statusCode).json({
+         status: err.status,
+         message: err.message
+      });
+   } else {
+      // Programming Error
+      res.status(500).json({
+         status: 'error',
+         message: 'Something went very wrong!'
+      });
+   }
+};
+
+const handleDuplicateFieldDB = (err: ErrorType) => {
+   const message = `Duplicate field value ${err.keyValue.email}. Please use another email!`;
+
+   return new AppError(message, 400);
+};
+
+export default (
+   err: ErrorType,
+   _req: Request,
+   res: Response,
+   _next: NextFunction
+) => {
+   err.statusCode = err.statusCode || 500;
+   err.status = err.status || 'error';
+
+   if (config.env === 'dev') {
+      sendErrorDev(err, res);
+   } else if (config.env === 'prod') {
+      let error: AppError = new AppError('Something went wrong!', 500);
+
+      if (err.code === 11000) {
+         error = handleDuplicateFieldDB(err);
+      }
+
+      sendErrorProd(error, res);
+   }
+};
