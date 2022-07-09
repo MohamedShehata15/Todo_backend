68563d1fda71e9225ec7906ea511de62d3b90180
mohamed159716 <mohamed159716@yahoo.com>
2022-07-09 23:52:45 +0200

    Added Get All Logged User's Todos

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -1,12 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 
 import Todo, { TodoDocument } from '../models/Todo';
+import User from '../models/User';
 import catchAsync from '../utils/catchAsync';
+import { UserRequest } from '../types/userRequest.types';
 
 class TodoController {
    add = catchAsync(
-      async (req: Request, res: Response, _next: NextFunction) => {
+      async (req: UserRequest, res: Response, _next: NextFunction) => {
+         const loggedUser = await User.findById(req.user?.id);
+
          const todo: TodoDocument = await Todo.create(req.body);
+
+         loggedUser?.todos.push(todo._id);
+         loggedUser?.save({ validateBeforeSave: false });
 
          res.status(201).json({
             status: 'success',
@@ -15,7 +22,18 @@ class TodoController {
       }
    );
 
-   getAll = () => {};
+   getAll = catchAsync(
+      async (req: UserRequest, res: Response, _next: NextFunction) => {
+         const todos = await User.findById(req.user?.id)
+            .select('-name -email -_id -__v')
+            .populate('todos');
+
+         res.status(200).json({
+            status: 'success',
+            todos: todos?.todos
+         });
+      }
+   );
 
    getOne = () => {};
 
