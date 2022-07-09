561a3139a26ef2fb96861acccf0c395483c8b4b3
mohamed159716 <mohamed159716@yahoo.com>
2022-07-09 15:29:13 +0200

    Added Protected Token Middleware

--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -1,10 +1,13 @@
 import { Router, IRouter } from 'express';
 
 import TodoController from '../controllers/todoController';
+import AuthController from '../controllers/authController';
 
 const todoController = new TodoController();
 
 const todoRoutes: IRouter = Router();
+
+todoRoutes.use(new AuthController().protect);
 
 todoRoutes.route('/').post(todoController.add);
 
