f357d1c83320a500ef69a2e6b935a7683ed27bfe
mohamed159716 <mohamed159716@yahoo.com>
2022-07-10 06:40:51 +0200

    Deleted Todo Id from user's todos during deleting todo

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
 import Todo, { TodoDocument } from '../models/Todo';
-import User from '../models/User';
+import User, { UserDocument } from '../models/User';
 import catchAsync from '../utils/catchAsync';
 import { UserRequest } from '../types/userRequest.types';
 import { AppError } from '../utils/appError';
@@ -64,8 +64,23 @@ class TodoController {
    );
 
    delete = catchAsync(
-      async (req: UserRequest, res: Response, _next: NextFunction) => {
-         await Todo.findByIdAndDelete(req.params.id);
+      async (req: UserRequest, res: Response, next: NextFunction) => {
+         const todo: TodoDocument | null = await Todo.findByIdAndDelete(
+            req.params.id
+         );
+
+         if (!todo)
+            return next(new AppError('No todo found with that ID', 404));
+
+         const loggedUser: UserDocument | null = await User.findById(
+            req.user?.id
+         );
+         if (loggedUser) {
+            loggedUser.todos = loggedUser?.todos.filter(
+               (t) => t.toString() !== todo._id.toString()
+            );
+            loggedUser.save({ validateBeforeSave: false });
+         }
 
          res.status(204).json({
             status: 'success'
