25031b5e66e8b77869920e26651a5cde5f8eecfd
mohamed159716 <mohamed159716@yahoo.com>
2022-07-11 18:48:28 +0200

    Added Morgan

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import todoRoutes from './routes/todoRoutes';
 import { AppError } from './utils/appError';
 import globalErrorHandler from './controllers/errorController';
 import cors from 'cors';
-import { morgan } from 'morgan';
+import morgan from 'morgan';
 
 const port = config.port || 4000;
 
